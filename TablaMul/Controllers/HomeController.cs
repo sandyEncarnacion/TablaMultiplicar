a8ed9ad2489d145443cb90ddb21a8d4832323d78
sandy <sandy-encarnacion@live.com>
2020-02-09 09:17:51 -0400

    2do commit

--- TablaMul/Controllers/HomeController.cs
+++ TablaMul/Controllers/HomeController.cs
@@ -12,7 +12,7 @@ namespace TablaMul.Controllers
     {
         public IActionResult Index()
         {
-            return View();
+            return View("Mytabla");
         }
 
         public IActionResult Privacy()
